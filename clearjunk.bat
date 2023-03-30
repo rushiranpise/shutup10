496888822f152c47838aeb521b4e60e9c61ab3de
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2023-03-30 08:10:16 +0530

    Update clearjunk.bat

--- clearjunk.bat
+++ clearjunk.bat
@@ -1,21 +1,8 @@
 del /f /s /q "%userprofile%\recent\*.*"
-
 del /f /s /q %windir%\prefetch\*.*
-
 del /s /f /q %windir%\temp\*
-
 del /s /f /q %USERPROFILE%\appdata\local\temp\*
-
 del /s /f /q %APPDATA%\Microsoft\Windows\Recent\*
-
-ipconfig /flushdns
-cd %homepath%\AppData\Local\Microsoft\Windows\Explorer
-
-taskkill /f /im explorer.exe
-del iconcache*
-
-explorer.exe
-
 del %windir%\KB*.log /f /q
 del /f /s /q %systemdrive%\*.tmp
 del /f /s /q %systemdrive%\*._mp
@@ -26,4 +13,13 @@ del /f /s /q %systemdrive%\*.old
 del /f /s /q %systemdrive%\recycled\*.*
 del /f /s /q %windir%\*.bak
 del /f /s /q "%userprofile%\local settings\temporary internet files\*.*" 
+
+ipconfig /flushdns
+cd %homepath%\AppData\Local\Microsoft\Windows\Explorer
+
+taskkill /f /im explorer.exe
+del iconcache*
+
+start explorer.exe
+
 exit
