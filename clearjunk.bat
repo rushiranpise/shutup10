f34b8ecc0353086fd7b3ff68c7f0a9d30646c38d
Rushi Ranpise <37355997+rushiranpise@users.noreply.github.com>
2024-03-09 11:51:25 +0530

    Update clearjunk.bat

--- clearjunk.bat
+++ clearjunk.bat
@@ -6,20 +6,19 @@ del /s /f /q %APPDATA%\Microsoft\Windows\Recent\*
 del %windir%\KB*.log /f /q
 del /f /s /q %systemdrive%\*.tmp
 del /f /s /q %systemdrive%\*._mp
-del /f /s /q %systemdrive%\*.log
 del /f /s /q %systemdrive%\*.gid
 del /f /s /q %systemdrive%\*.chk
 del /f /s /q %systemdrive%\*.old
 del /f /s /q %systemdrive%\recycled\*.*
 del /f /s /q %windir%\*.bak
 del /f /s /q "%userprofile%\local settings\temporary internet files\*.*" 
-
+del /s /f /q C:\Users\Public\Desktop\settings.json
 ipconfig /flushdns
 cd %homepath%\AppData\Local\Microsoft\Windows\Explorer
-
 taskkill /f /im explorer.exe
 del iconcache*
-
-start explorer.exe
-
+taskkill /IM LegionFanControl.exe /F
+taskkill /IM legion-kb-rgb.exe /F
+explorer.exe
+del /s /f /q C:\Users\Public\Desktop\settings.json
 exit
